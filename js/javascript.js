90102cf0605d53d1c121c3bebda2e907327534f3
nnombarasco <nicolas.nombarasco@gmail.com>
2022-05-10 17:59:29 -0300

    arreglo de codigo javascript y agrego de class + metodos

--- js/javascript.js
+++ js/javascript.js
@@ -1,18 +1,66 @@
 const userAdmin = "Admin";
 const passAdmin = "admin123";
-const articulo_1 = {
-    nombre: "Nueces",
-    valor: 200,
-    cantidad : 1000,
-    disponibilidad : true
-}
-const articulo_2 = {
-    nombre: "Avellanas",
-    valor: 150,
-    cantidad : 600,
-    disponibilidad : true
-}
 let opcion;
+let carrito = [];
+
+function ValidarNumero(num){
+    if((!isNaN(num) && num != "" && num != null)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function total(cant, prec){
+    return cant * prec;
+}
+
+class Producto{
+    constructor (name, price, amount, available){
+        this.nombre = name;
+        this.precio = price;
+        this.cantidad = amount;
+        this.disponible = available;
+    }
+
+    comprar = function (unidades){
+        if(ValidarNumero(unidades)){
+            if(this.cantidad <= 0 || this.cantidad < unidades){
+                this.available = false;
+                console.log("No hay stock suficiente.");
+            }else{
+                console.log("se vende");
+                let compra = {
+                    articulo: this.nombre,
+                    precio: this.precio,
+                    cantidad: unidades,
+                    subTotal: total(unidades, this.precio)
+                };
+                this.cantidad -= unidades;
+                carrito.push(compra);
+                return carrito;
+            }
+        }else{
+            console.log("No ingreso unidades correctas.");
+        }
+    }
+}
+
+function finalizaCompra (){
+    let total = 0;
+    for (const item of carrito) {
+        total =  total + parseInt(item.subTotal);
+        console.log("Su compra realizada fue de " + item.articulo + " por " + item.cantidad + "Kilos.");
+    }
+    if(total == 0){
+        alert("No ha realizado ninguna compra.")
+    }else{
+        alert("El total a pagar es de: $" + total);
+    }
+}
+
+const articulo_1 = new Producto("Nueces", 200, 1000, true);
+const articulo_2 = new Producto("Avellanas", 150, 600, true);
 
 function validateLogin(){
     
@@ -48,54 +96,5 @@ function showPassword(){
         tipo.type = "text";
     }else{
         tipo.type = "password";
-    }
-}
-
-function ValidarNumero(){
-    if((!isNaN(opcion) && opcion != "" && opcion != null)){
-        return true;
-    }else{
-        return false;
-    }
-}
-
-function total(cant, prec){
-    return cant * prec;
-}
-
-function comprar(){
-    opcion = prompt("por favor, ingrese 1 si desea comprar Nueces y 2 si desea comprar Avellanas");
-    if(ValidarNumero(opcion)){         //aca validamos que ingrese un numero si o si
-        if(opcion == 1){                                                        //compramos nueces
-            console.log("quiere nueces");
-            alert("Hola!, actualmente la cantidad de " + articulo_1.nombre + " que tenemos son: " + articulo_1.cantidad + "Kilos y su valor por kilo es de: " + articulo_1.valor);
-            let kilos = prompt("Por favor ingrese la cantida de kilos que desea: ");
-            if(ValidarNumero(kilos)){
-                console.log("quiere: " + kilos + "kilos");
-                if(kilos > articulo_1.cantidad){
-                    alert("Error, no puede comprar mas de la cantidad de stock!");
-                }else{
-                    alert("Bien, su producto fue cargado con exito!");
-                    alert("el total de su compra por " + kilos + " Kilos de " + articulo_1.nombre + " es de: $" + total(kilos, articulo_1.valor)) ;
-                }
-            }
-        }if (opcion == 2) {
-            console.log("quiere avellanas");
-            alert("Hola!, actualmente la cantidad de " + articulo_2.nombre + " que tenemos son: " + articulo_2.cantidad + "Kilos y su valor por kilo es de: " + articulo_2.valor);
-            let kilos = prompt("Por favor ingrese la cantida de kilos que desea: ");
-            if(ValidarNumero(kilos)){
-                console.log("quiere: " + kilos + "kilos");
-                if(kilos > articulo_2.cantidad){
-                    alert("Error, no puede comprar mas de la cantidad de stock!");
-                }else{
-                    alert("Bien, su producto fue cargado con exito!");
-                    alert("el total de su compra por " + kilos + " Kilos de " + articulo_2.nombre + " es de: $" + total(kilos, articulo_2.valor)) ;
-                }
-            }
-        } if(opcion > 2 || opcion < 1){
-            alert("Opcion invalida!");
-        }
-    }else{
-        alert("Por favor, ingresa una opcion valida, chau!");
     }
 }
